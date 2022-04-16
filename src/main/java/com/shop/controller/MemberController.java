c3f4ac21604a3ccae3a25eb3ef23e7b13aabfaca
jungyin <noelcoolblue@gmail.com>
2022-04-16 23:01:02 +0900

    validation 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -25,10 +28,25 @@ public class MemberController {
         return "member/memberForm";
     }
 
+//    @PostMapping(value = "/new")
+//    public String memberForm(MemberFormDto dto) {
+//        Member member = Member.createMember(dto, passwordEncoder);
+//        memberService.saveMember(member);
+//        return "redirect:/";
+//    }
+
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto dto) {
-        Member member = Member.createMember(dto, passwordEncoder);
-        memberService.saveMember(member);
+    public String newMember(@Valid MemberFormDto dto, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+        try {
+            Member member = Member.createMember(dto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch(IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 }
