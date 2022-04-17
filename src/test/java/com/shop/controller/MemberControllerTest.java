5aed85063a6596d933a6ef533f1e55c3a400ea66
jungyin <noelcoolblue@gmail.com>
2022-04-17 13:48:01 +0900

    로그인 실패 테스트 추가

--- src/test/java/com/shop/controller/MemberControllerTest.java
+++ src/test/java/com/shop/controller/MemberControllerTest.java
@@ -56,4 +56,18 @@ class MemberControllerTest {
                 andExpect(SecurityMockMvcResultMatchers.authenticated());
     }
 
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception {
+        String email = "test@jungyin.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(
+                        formLogin().userParameter("email").
+                                loginProcessingUrl("/members/login").
+                                user(email).
+                                password("12345")).
+                andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
 }
