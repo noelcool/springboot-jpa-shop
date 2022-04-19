afae0696932175e934c812eef139392f3dce7721
jungyin <noelcoolblue@gmail.com>
2022-04-19 20:33:52 +0900

    add AuthenticationEntryPoint interface

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,11 +31,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 logout().
                 logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")).
                 logoutSuccessUrl("/");
+
+        http.authorizeRequests().
+                mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll().
+                mvcMatchers("/admin/**").hasRole("ADMIN").
+                anyRequest().authenticated();
+
+        http.exceptionHandling().
+                authenticationEntryPoint(new CustomAuthenticationEntryPoint());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() { // 암호 저장시 해시 함수로 암호화
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Override
